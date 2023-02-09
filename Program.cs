8d3012984f82c9b4a76721f72f59e8e2ed97f916
raonatesh89 <124659633+raonatesh89@users.noreply.github.com>
2023-02-09 10:41:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json","My API V1");
+    c.RoutePrefix = string.Empty;
+});
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
